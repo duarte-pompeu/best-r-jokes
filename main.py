ab4d5720d97af2ee46673adb0e3350b60d9bd68d
duarte-pompeu <pnt12@live.com.pt>
2016-03-04 22:52:57 +0000

    clean up

--- main.py
+++ main.py
@@ -23,67 +23,6 @@ TWITTER_LIMIT = 140
 DB = sqlite3.connect("./jokes_db")
 CURSOR = DB.cursor()
 
-def log(msg):
-	if LOGGER:
-		print msg
-
-def id_in_db(post_id):
-	CURSOR.execute("select id from submissions where id=?", [post_id])
-	result =  CURSOR.fetchone()
-
-	if result:
-		return True
-	else:
-		return False
-
-
-def save_in_db(post_id, post_url):
-	sql = "insert into submissions values(?, ?)"
-	log("SQL: " + sql + "\n" + post_id + ", " + post_url + "\n")
-	CURSOR.execute(sql, [post_id, post_url])
-
-
-def get_long_url(post_id):
-	return "https://reddit.com/r/jokes/" + post_id
-
-
-def get_url(post_id):
-	return "redd.it/" + post_id
-
-
-def format_twitter_all(post_id, title, text):
-	return "%s\n\n%s\n\n%s" %(title, text, get_url(post_id))
-
-
-def format_twitter_title(post_id, title):
-	return "%s\n\n%s" %(title, get_url(post_id))
-
-
-def format_twitter_url(post_id):
-	return "%s" % get_url(post_id)
-
-
-def tweet(tweet_text):
-	text = tweet_text.encode('utf-8')
-	print "TWEETING: " + text
-
-	if not TESTING:
-		try:
-			return subprocess.call([config.twitter_path, "set", text])
-		except Exception as e:
-			print str(e)
-			return 0
-	else:
-		return 0
-
-def publish_facebook(title, text, url):
-	token = secrets.FB_TOKEN
-	api = facebook.GraphAPI(token)
-	msg = title.encode("utf-8") + "\n\n" + text.encode("utf-8") + "\n\n" + url
-	api.put_wall_post(msg)
-	
-	return
-
 def main():
 	# https://praw.readthedocs.org/en/v3.0.0/pages/writing_a_bot.html#writing-a-bot
 	r = praw.Reddit('PRAW related-question monitor by /u/_Daimon_ v 1.0. '
@@ -122,13 +61,81 @@ def main():
 		else:
 			if score >= LONG_TEXT_MIN_SCORE and not feed.item_in_feed("https://" + get_url(post_id)):
 				feed.add_entry(title, "https://" + get_long_url(post_id), text)
-				publish_facebook(title, text, get_long_url(post_id))
+				publish_facebook(title, text, "https://" + get_long_url(post_id))
 				print "NEW RSS FEED ===============\n" + title + " - " + get_url(post_id)
 
 	if not TESTING:
 		DB.commit()
 
 	feed.close()
+
+
+def tweet(tweet_text):
+	text = tweet_text.encode('utf-8')
+	print "TWEETING: " + text
+
+	if not TESTING:
+		try:
+			return subprocess.call([config.twitter_path, "set", text])
+		except Exception as e:
+			print str(e)
+			return 0
+	else:
+		return 0
+
+def publish_facebook(title, text, url):
+	token = secrets.FB_TOKEN
+	api = facebook.GraphAPI(token)
+	msg = title.encode("utf-8") + "\n\n" + text.encode("utf-8") + "\n\n" + url
+	api.put_wall_post(msg)
+
+	return
+
+
+def id_in_db(post_id):
+	CURSOR.execute("select id from submissions where id=?", [post_id])
+	result =  CURSOR.fetchone()
+
+	if result:
+		return True
+	else:
+		return False
+
+
+def save_in_db(post_id, post_url):
+	sql = "insert into submissions values(?, ?)"
+	log("SQL: " + sql + "\n" + post_id + ", " + post_url + "\n")
+	CURSOR.execute(sql, [post_id, post_url])
+
+
+
+
+def log(msg):
+	if LOGGER:
+		print msg
+
+
+def get_long_url(post_id):
+	return "reddit.com/r/jokes/" + post_id
+
+
+def get_url(post_id):
+	return "redd.it/" + post_id
+
+
+def format_twitter_all(post_id, title, text):
+	return "%s\n\n%s\n\n%s" %(title, text, get_url(post_id))
+
+
+def format_twitter_title(post_id, title):
+	return "%s\n\n%s" %(title, get_url(post_id))
+
+
+def format_twitter_url(post_id):
+	return "%s" % get_url(post_id)
+
+
+
 
 if __name__ == "__main__":
 	main()
